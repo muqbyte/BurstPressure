fe72b0f46d3189e3d40ae2a5547d49d9aa644b35
szulhadydev <szulhady.dev@gmail.com>
2024-07-22 18:44:32 +0800

    update

--- FrontEndMachinelearning/src/config.jsx
+++ FrontEndMachinelearning/src/config.jsx
@@ -1,8 +1,8 @@
 
 // SERVER
-// export const API_ENDPOINT_ML = "https://ml.eagleattech.com";
-// export const API_ENDPOINT_BACK = "https://backdoor.eagleattech.com";
+export const API_ENDPOINT_ML = "https://ml.eagleattech.com";
+export const API_ENDPOINT_BACK = "https://backdoor.eagleattech.com";
 
 // LOCALHOST
-export const API_ENDPOINT_ML = "http://localhost:80";
-export const API_ENDPOINT_BACK = "http://localhost:5500";
+// export const API_ENDPOINT_ML = "http://localhost:80";
+// export const API_ENDPOINT_BACK = "http://localhost:5500";
